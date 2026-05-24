60336170fc36e192f7aa774db6b3e985dda22471
Jilvin Jacob <jilvinjacob@gmail.com>
2026-05-25 00:43:33 +0530

    Add github action to build using static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
-<div class="prose pl-6 pt-6">
-    <h1 class="mb-0">
-        Welcome to
-        <span class="text-[#4f46e5]">
-            MunchBear
-        </span>
-    </h1>
-    <p class="mt-0">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation for SvelteKit.</p>
+<div class="prose pt-6 pl-6">
+	<h1 class="mb-0">
+		Welcome to
+		<span class="text-[#4f46e5]"> MunchBear </span>
+	</h1>
+	<p class="mt-0">
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation for
+		SvelteKit.
+	</p>
 </div>
