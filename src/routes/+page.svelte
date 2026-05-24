75658d8c6927deab8b8e1380adecd1e0aa230d5f
Jilvin Jacob <jilvinjacob@gmail.com>
2026-05-24 21:54:34 +0530

    Add minor changes to landing text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="prose pl-6 pt-6">
+    <h1 class="mb-0">
+        Welcome to
+        <span class="text-[#4f46e5]">
+            MunchBear
+        </span>
+    </h1>
+    <p class="mt-0">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation for SvelteKit.</p>
+</div>
