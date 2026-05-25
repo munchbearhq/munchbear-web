a75f13f48ceabda6a6f1f5c9a11c0a3bce278b3d
Jilvin Jacob <jilvinjacob@gmail.com>
2026-05-26 00:42:14 +0530

    Fix build error and add build check to pre-push hook

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon-colored-transparent.png';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
